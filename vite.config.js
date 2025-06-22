bbc86269666e0b30695339546d0d12f0cd6c191b
Ibarahona73 <igyt2015@gmail.com>
2025-06-21 22:27:26 -0500

    diseño

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  css: {
-    postcss: {
-      configFile: 'postcss.config.cjs',
-    },
-  },
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
