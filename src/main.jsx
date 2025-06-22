738c8cac879918c969df58074edab0080ee11039
Ibarahona73 <igyt2015@gmail.com>
2025-06-21 22:30:26 -0500

    diseño

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
